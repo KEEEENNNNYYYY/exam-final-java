fe800ce03d93e1a08df3b891eb18c1fe4c35ef3c
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-09-14 16:47:07 +0200

    update:new files

--- studentAttendance/src/main/java/hei/school/kenny/attendance/application/Main.java
+++ studentAttendance/src/main/java/hei/school/kenny/attendance/application/Main.java
@@ -1,4 +1,4 @@
-package hei.school.kenny.gestionD_absence.application;
+package hei.school.kenny.attendance.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,11 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({
-        "hei.school.kenny.gestionD_absence.controller"
+        "hei.school.kenny.attendance.controller",
+        "hei.school.kenny.attendance.DAO",
+        "hei.school.kenny.attendance.service",
+        "hei.school.kenny.attendance.model"
+
 })
 public class Main {
 
