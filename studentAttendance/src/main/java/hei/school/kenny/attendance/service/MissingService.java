4a6c1e2e9dfc0deed24d62724d6e10a175e9337d
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-09-15 15:23:38 +0200

    feat : new missing service

---
+++ studentAttendance/src/main/java/hei/school/kenny/attendance/service/MissingService.java
@@ -0,0 +1,21 @@
+package hei.school.kenny.attendance.service;
+
+import hei.school.kenny.attendance.DAO.MissingListDAO;
+import hei.school.kenny.attendance.model.MissingList;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class MissingService {
+    private final MissingListDAO missingListDAO;
+
+    public MissingService(MissingListDAO missingListDAO) {
+        this.missingListDAO = missingListDAO;
+    }
+
+    public List<MissingList> getMissingListByDate(Date date) {
+        return missingListDAO.getMissingListByDate(date);
+    }
+}
