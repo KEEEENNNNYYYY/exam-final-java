af0e97388a4e84fa2643829c692c1b455c96143a
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-09-15 16:53:31 +0200

    feat : added missing by id on controller

--- studentAttendance/src/main/java/hei/school/kenny/attendance/controller/ShowMissingController.java
+++ studentAttendance/src/main/java/hei/school/kenny/attendance/controller/ShowMissingController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 @RestController
 public class ShowMissingController {
     private final MissingService missingService;
@@ -17,8 +16,17 @@ public class ShowMissingController {
     }
 
     @GetMapping("/students/missing")
-    public List<MissingList> showAllMissing(@RequestParam("date") String date) {
-        java.util.Date utilDate = java.sql.Date.valueOf(date);
-        return missingService.getMissingListByDate(utilDate);
+    public List<MissingList> showMissing(
+            @RequestParam(value = "date", required = false) String date,
+            @RequestParam(value = "id", required = false) String id) {
+
+        if (date != null) {
+            java.util.Date utilDate = java.sql.Date.valueOf(date);
+            return missingService.getMissingListByDate(utilDate);
+        } else if (id != null) {
+            return missingService.getMissingListById(id);
+        } else {
+            throw new IllegalArgumentException("Either 'date' or 'id'");
+        }
     }
 }
