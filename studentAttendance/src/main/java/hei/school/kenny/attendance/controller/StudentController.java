affb1d1c4083d71b6ddad07becba7e2ba4a369b4
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-09-16 19:59:04 +0200

    feat : new postMapping endpoint for student post

--- studentAttendance/src/main/java/hei/school/kenny/attendance/controller/StudentController.java
+++ studentAttendance/src/main/java/hei/school/kenny/attendance/controller/StudentController.java
@@ -1,23 +1,32 @@
 package hei.school.kenny.attendance.controller;
 
+import hei.school.kenny.attendance.model.MissingListRequest;
+import hei.school.kenny.attendance.model.NewStudentRequest;
 import hei.school.kenny.attendance.model.Student;
 import hei.school.kenny.attendance.service.StudentService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-public class ShowStudentController {
+public class StudentController {
 
     private final StudentService studentService;
 
-    public ShowStudentController(StudentService studentService) {
+    public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
-    @GetMapping("/students")
+    @GetMapping("/student/all")
     public List<Student> showAllStudent() {
         return studentService.getAllStudent();
     }
+    @PostMapping("/student/add")
+    public void addStudent(@RequestBody NewStudentRequest newStudentRequest) {
+        studentService.addStudent(newStudentRequest);
+    }
+
 }
