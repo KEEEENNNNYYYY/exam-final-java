a1f189d9c1b38b97658fcd6587deff597025e8ef
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-09-16 09:38:35 +0200

    refactor : remove id

--- studentAttendance/src/main/java/hei/school/kenny/attendance/model/Subject.java
+++ studentAttendance/src/main/java/hei/school/kenny/attendance/model/Subject.java
@@ -1,29 +1,14 @@
 package hei.school.kenny.attendance.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Subject {
     private String name;
-    @JsonInclude
     private int totalHours;
     private String teacher;
-    @JsonInclude
-    private int id;
 
     public Subject() {
-        this.id = id;
         this.name = name;
         this.teacher = teacher;
         this.totalHours = totalHours;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
