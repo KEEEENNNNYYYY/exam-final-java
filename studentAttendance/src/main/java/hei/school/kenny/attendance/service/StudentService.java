99a6a5e6cfa03bcedd206aef3775f8d4e2786ef1
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-09-16 19:59:56 +0200

    feat : new service for adding new student

--- studentAttendance/src/main/java/hei/school/kenny/attendance/service/StudentService.java
+++ studentAttendance/src/main/java/hei/school/kenny/attendance/service/StudentService.java
@@ -1,19 +1,22 @@
 package hei.school.kenny.attendance.service;
 
 import java.util.List;
-import hei.school.kenny.attendance.DAO.ShowStudentDAO;
+import hei.school.kenny.attendance.DAO.StudentDAO;
+import hei.school.kenny.attendance.model.NewStudentRequest;
 import hei.school.kenny.attendance.model.Student;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
-    private final ShowStudentDAO showStudentDAO;
+    private final StudentDAO studentDAO;
 
-    public StudentService(ShowStudentDAO showStudentDAO) {
-        this.showStudentDAO = showStudentDAO;
+    public StudentService(StudentDAO studentDAO) {
+        this.studentDAO = studentDAO;
     }
 
     public List<Student> getAllStudent() {
-        return showStudentDAO.fetchAllStudent();
+        return studentDAO.fetchAllStudent();
     }
+
+    public void addStudent(NewStudentRequest newStudentRequest){studentDAO.addStudent(newStudentRequest);}
 }
