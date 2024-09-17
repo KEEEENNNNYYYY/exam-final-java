0df033697df423e8e68a6843898ded6c12edd910
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-09-17 21:23:32 +0200

    feat : new function for updating course on late list

--- studentAttendance/src/main/java/hei/school/kenny/attendance/DAO/LateListDAO.java
+++ studentAttendance/src/main/java/hei/school/kenny/attendance/DAO/LateListDAO.java
@@ -236,6 +236,34 @@ public class LateListDAO implements Serializable {
         }
     }
 
+    public void updateSubjectLateList(String studentId, String oldSubject, String newSubject) {
+        Connection conn = connectToDb();
+
+        if (conn != null) {
+            String query = "UPDATE late_list SET subject_id = ? WHERE subject_id = ? AND student_id = ?";
+            try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+                pstmt.setString(1, newSubject);
+                pstmt.setString(2, oldSubject);
+                pstmt.setString(3, studentId);
+
+                int rowsAffected = pstmt.executeUpdate();
+                System.out.println("Rows updated: " + rowsAffected);
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Error updating missing list", e);
+            } finally {
+                try {
+                    if (conn != null && !conn.isClosed()) {
+                        conn.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 
     private Student getStudentById(String studentId) {
         Student student = null;
