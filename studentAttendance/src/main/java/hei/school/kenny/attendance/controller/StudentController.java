18b62989860b48f44ee1992ae6e090f98eb57bf9
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-09-17 08:41:25 +0200

    feat : new end point search by cor

--- studentAttendance/src/main/java/hei/school/kenny/attendance/controller/StudentController.java
+++ studentAttendance/src/main/java/hei/school/kenny/attendance/controller/StudentController.java
@@ -1,13 +1,11 @@
 package hei.school.kenny.attendance.controller;
 
+import hei.school.kenny.attendance.model.MissingList;
 import hei.school.kenny.attendance.model.MissingListRequest;
 import hei.school.kenny.attendance.model.NewStudentRequest;
 import hei.school.kenny.attendance.model.Student;
 import hei.school.kenny.attendance.service.StudentService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,9 +22,22 @@ public class StudentController {
     public List<Student> showAllStudent() {
         return studentService.getAllStudent();
     }
+
+    @GetMapping("/students/search")
+    public List<Student> showCored(
+            @RequestParam(value = "cored", required = false) Boolean cored
+    ) {
+        if (cored != null) {
+            return studentService.getStudentListByCor(cored);
+        } else {
+            throw new IllegalArgumentException("Parameter 'cored' must be provided");
+        }
+    }
+
+
     @PostMapping("/student/add")
     public void addStudent(@RequestBody NewStudentRequest newStudentRequest) {
         studentService.addStudent(newStudentRequest);
     }
-
+    
 }
