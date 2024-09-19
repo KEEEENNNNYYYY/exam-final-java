254ad4e6299169aafc17bccf0ee70d8dbd31f980
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-09-19 12:44:56 +0200

    feat : new endpoint to jsutify missing

--- studentAttendance/src/main/java/hei/school/kenny/attendance/controller/MissingController.java
+++ studentAttendance/src/main/java/hei/school/kenny/attendance/controller/MissingController.java
@@ -5,6 +5,9 @@ import hei.school.kenny.attendance.model.MissingListRequest;
 import hei.school.kenny.attendance.service.MissingService;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 @RestController
 public class MissingController {
@@ -51,6 +54,23 @@ public class MissingController {
     ) {
         missingService.updateMissingListBySubject(studentId, oldValue, newValue);
     }
+
+    @PutMapping("/students/missing/justify")
+    public void updateJustification(
+            @RequestParam(value = "studentId", required = true) String studentId,
+            @RequestParam(value = "subject", required = false) String subject,
+            @RequestParam(value = "date", required = false) String date
+    ) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Date parsedDate = sdf.parse(date);
+            missingService.justifyMissing(studentId, subject, parsedDate);
+        } catch (ParseException e) {
+            throw new RuntimeException("Invalid date format: " + e.getMessage());
+        }
+    }
+
+
 
     @DeleteMapping("/students/missing/del")
     public void updateMissingList(
