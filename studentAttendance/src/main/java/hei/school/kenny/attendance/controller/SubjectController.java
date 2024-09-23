5e726a012798b0c41b4ec5292980d47db6c89b2e
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-09-23 11:16:54 +0200

    feat : new endpoint fetch all subject

--- studentAttendance/src/main/java/hei/school/kenny/attendance/controller/SubjectController.java
+++ studentAttendance/src/main/java/hei/school/kenny/attendance/controller/SubjectController.java
@@ -1,15 +1,25 @@
 package hei.school.kenny.attendance.controller;
 
 import hei.school.kenny.attendance.model.NewSubjectRequest;
+import hei.school.kenny.attendance.model.Student;
+import hei.school.kenny.attendance.model.Subject;
 import hei.school.kenny.attendance.service.SubjectService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SubjectController {
 
     private final SubjectService subjectService;
+
+    @GetMapping("/subject/all")
+    public List<Subject> showAllSubject() {
+        return subjectService.getAllSubjects();
+    }
 
     public SubjectController(SubjectService subjectService) {
         this.subjectService = subjectService;
