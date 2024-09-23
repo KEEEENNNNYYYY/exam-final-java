209312cf694ee071d9404a94d4e607123adac370
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2024-09-23 14:23:09 +0200

    update : new function update subject name

--- studentAttendance/src/main/java/hei/school/kenny/attendance/DAO/SubjectDAO.java
+++ studentAttendance/src/main/java/hei/school/kenny/attendance/DAO/SubjectDAO.java
@@ -188,4 +188,31 @@ public class SubjectDAO {
             }
         }
     }
+
+    public void updateName(String value,String id) {
+        Connection conn = connectToDb();
+
+        if (conn != null) {
+            String query = "UPDATE subject SET name = ? WHERE name = ?";
+            try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+                pstmt.setString(1, value);
+                pstmt.setString(2, id);
+
+                int rowsAffected = pstmt.executeUpdate();
+                System.out.println("Rows updated: " + rowsAffected);
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Error updating missing list", e);
+            } finally {
+                try {
+                    if (conn != null && !conn.isClosed()) {
+                        conn.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
